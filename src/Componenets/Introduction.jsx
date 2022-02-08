56f02ace3e0b25849746cf70713ffa3387fd27ca
sivacreator <93380641+sivacreator@users.noreply.github.com>
2022-02-08 11:13:54 +0530

    first

---
+++ src/Componenets/Introduction.jsx
@@ -0,0 +1,18 @@
+
+import "./Introduction.css"
+
+export default function Introduction(){
+return(
+     <nav id="navbar">
+          <header className="header">
+               <h1 className="logo">Sivanandam Gorantla</h1>
+                     <ul className="main-nav">
+                         <li>Home</li>
+                         <li>About</li>
+                         <li>Portfolio</li>
+                         <li>Contact</li>
+                    </ul>
+	     </header> 
+     </nav>
+)
+}
