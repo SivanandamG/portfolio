c9155c433baa1fe1746d9314b5bd26e999a72d51
sivacreator <93380641+sivacreator@users.noreply.github.com>
2022-02-10 10:43:38 +0530

    ..

--- src/Componenets/Contact/Contactme.jsx
+++ src/Componenets/Contact/Contactme.jsx
@@ -23,9 +23,9 @@ export default function Contact({children}){
       <input id="submitbtn" type="submit" value="Send" /></form>
           </div>
                <div id="techstack-icon">
-                        <a href="https://github.com/SivanandamG" target="_blank"> <i  style={{fontSize:"30px", color: 'black' }}  className="fab fa-github"></i></a>
-                        <a href="https://www.linkedin.com/in/sivanandam-gorantla-878a511b7/" target="_blank"> <i style={{fontSize:"30px", color: 'black'}} className="fab fa-linkedin"></i></a>
-                        <a href="https://twitter.com/Sivanan17585739" target="_blank"> <i style={{fontSize:"30px", color: 'black' }}  className="fab fa-twitter"></i></a>
+                        <a href="https://github.com/SivanandamG" target="_blank" rel="noreferrer"> <i  style={{fontSize:"30px", color: 'black' }}  className="fab fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/sivanandam-gorantla-878a511b7/" target="_blank" rel="noreferrer"> <i style={{fontSize:"30px", color: 'black'}} className="fab fa-linkedin"></i></a>
+                        <a href="https://twitter.com/Sivanan17585739" target="_blank" rel="noreferrer"> <i style={{fontSize:"30px", color: 'black' }}  className="fab fa-twitter"></i></a>
                </div>
                <p>Location: Guntur,Andhra Pradesh</p>
                <p>Mobile: +91 8106251562</p>
