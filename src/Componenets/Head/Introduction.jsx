5b801b6c471fa1f269b070789380ece9ec90903c
sivacreator <93380641+sivacreator@users.noreply.github.com>
2022-02-11 15:38:14 +0530

    ......

--- src/Componenets/Head/Introduction.jsx
+++ src/Componenets/Head/Introduction.jsx
@@ -9,9 +9,9 @@ return(
                <h1 className="logo">Sivanandam Gorantla</h1>
                      <ul className="main-nav">
                          <li >About</li>
-                         <li onClick={()=>{skills.current.scrollIntoView()}}>skills</li>
-                         <li onClick={()=>{projects.current.scrollIntoView()}}>Projects</li>
-                         <li onClick={()=>{contact.current.scrollIntoView()}}>Contact</li>
+                         <li onClick={()=>{skills.current.scrollIntoView({behavior: "smooth"})}}>skills</li>
+                         <li onClick={()=>{projects.current.scrollIntoView({behavior: "smooth"})}}>Projects</li>
+                         <li onClick={()=>{contact.current.scrollIntoView({behavior: "smooth"})}}>Contact</li>
                     </ul>
 	     </header>
      </nav>
